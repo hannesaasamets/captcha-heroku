20eac28269049a3a507bc98cacd40e9ee9ce2a1b
hannes <hannes@aasamets.com>
2020-04-29 23:57:00 +0300

    Set explicit default content-type.

--- server.js
+++ server.js
@@ -27,6 +27,9 @@ app.post('/go-verify', async (req, res) => {
 
     const response = await fetch('https://www.google.com/recaptcha/api/siteverify', {
       method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
       body,
     });
     const json = await response.json(); // expecting a json response
