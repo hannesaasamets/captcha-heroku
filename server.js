eb886f4845d4abb75e86ce42b11cb0e51817f615
hannes.aasamets <hannes.aasamets@bigbank.ee>
2020-05-03 22:39:00 +0300

    Install mongodb.

--- server.js
+++ server.js
@@ -4,6 +4,25 @@ const path = require('path');
 const {PORT = 3333} = process.env;
 const app = express();
 const fetch = require('node-fetch');
+const { MongoClient } = require('mongodb');
+
+console.log('process.env.MONGODB_URI.length', process.env.MONGODB_URI.length);
+
+MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+  if (err) throw err;
+  console.log('⛱ db', db);
+
+  const dbo = db.db("heroku_7p79fwbh");
+  const myobj = { name: "Company Inc", address: "Highway 37" };
+  dbo.collection("visits").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
+});
+
+
+
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
